531dff58c495e10a840bd208f1eb493f9ecdfd47
Emme313 <mnvalentine313@gmail.com>
2019-12-19 13:43:27 -0800

    Lesson 46

---
+++ chapter_6_the-DOM/sandbox-46.js
@@ -0,0 +1,14 @@
+// this will grab the first p tag and ignore the rest of them
+// const para = document.querySelector('div.error')
+// console.log(para);
+
+// grabbing multiple elements
+const paras = document.querySelectorAll('p');
+// paras.forEach(para => {
+//     console.log(para);
+// });
+const errors = document.querySelectorAll('.error');
+
+// console.log(paras[0]);
+console.log(errors);
+
