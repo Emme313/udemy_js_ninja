684d3a8c43d67264b36ae3228283e6ce63278a03
Melaina Valentine <emme313@Melainas-MacBook-Pro.local>
2020-01-07 14:57:22 -0800

    Lessons 49 & 50

---
+++ chapter_6_the-DOM/sandbox-49.js
@@ -0,0 +1,11 @@
+const link = document.querySelector('a');
+
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk')
+link.innerHTML = 'the net ninja website';
+
+const mssg = document.querySelector('p');
+
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');
+mssg.setAttribute('style', 'color: green;');
