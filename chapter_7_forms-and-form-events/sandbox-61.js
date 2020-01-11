b4253c365c238cb099a44d0d820c08bbbc01a8ce
Melaina Valentine <emme313@Melainas-MacBook-Pro.local>
2020-01-10 23:28:51 -0800

    Lessons 59 & 61

---
+++ chapter_7_forms-and-form-events/sandbox-61.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.signup-form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(form.username.value);
+});
+
+// testing RegEx
+
+const username = 'emmmey';
+const pattern = /^[a-z]{6,}$/;
+
+// let result = pattern.test(username);
+// console.log(result);
+
+// if(result){
+//     console.log('regex test passed');
+// } else {
+//     console.log('regex test failed');
+// }
+
+let result = username.search(pattern);
+console.log(result);
