938c159b8ae5d1df2fe1c5781c630c9c4aa89ae6
Melaina Valentine <mnvalentine313@gmail.com>
2020-01-14 12:20:32 -0800

    Ninja Quiz, Lessons 69 & 70

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -16,6 +16,35 @@ form.addEventListener('submit', event => {
         }
     });
     // show results on page
-    result.querySelector('span').textContent = `${score}%`;
+    scrollTo(0,0)
+    
     result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++
+        }
+    }, 10)
 });
+
+// window object (global object, typically inferred)
+// window.console.log('hello');
+// console.log(window.document.querySelector('form'));
+// window.alert('hello');
+
+// setTimeout(() => {
+//     alert('hello ninjas')
+// }, 3000)
+
+// let i = 0;
+// const timer = setInterval(() => {
+//     console.log('hello');
+//     i++
+//     if(i === 5){
+//         clearInterval(timer);
+//     }
+// }, 1000);
