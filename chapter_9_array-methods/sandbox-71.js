725f92b27b2515f2be6ffac288d517ab8f575994
Melaina Valentine <mnvalentine313@gmail.com>
2020-01-14 14:02:51 -0800

    Lessons 71 & 72

---
+++ chapter_9_array-methods/sandbox-71.js
@@ -0,0 +1,24 @@
+const scores = [10, 30, 15, 25, 40, 5];
+
+// const filtered = scores.filter(score) => {
+//     return score > 20;
+// })
+
+// console.log(filtered);
+
+const users = [
+    {name: 'shaun', premium: true},
+    {name: 'yoshi', premium: false},
+    {name: 'mario', premium: false},
+    {name: 'chun-li', premium: true}
+];
+
+// array method to only get premium users
+// const premiumUsers = users.filter(user => {
+//     return user.premium;
+// });
+
+const premiumUsers = users.filter(user => user.premium);
+
+console.log(premiumUsers);
+
